07ea034512c59438e4faa67114c76364ee8890b5
Shaun Harrison <170037375+sharrisondev@users.noreply.github.com>
2025-10-23 19:11:33 +0000

    Solution

--- bank_account.hpp
+++ bank_account.hpp
@@ -10,26 +10,47 @@ private:
 public:
     // Constructor
     BankAccount(){
-        // TODO: Implement the constructor
+        // Implement the constructor
+        accountNumber = "";
+        accountHolderName = "";
+        balance = 0.0;
     }
 
     BankAccount(const std::string& accNumber, const std::string& accHolder, double initialBalance){
-        // TODO: Implement the constructor
+        // Implement the constructor
+        accountNumber = accNumber;
+        accountHolderName = accHolder;
+        balance = initialBalance;
     }
 
     // Deposit method
     void Deposit(double amount) {
-        // TODO: Implement the Deposit method
+        // Implement the Deposit method
+        if(amount > 0){
+            balance += amount;
+            std::cout << "Deposited " << amount << " into account.\n";
+        } else {
+            std::cout << "The deposit must not be a negative amount.\n";
+        }
     }
 
     // Withdraw method
     void Withdraw(double amount) {
-        // TODO: Implement the Deposit method
+        // Implement the Deposit method
+        if(amount <= balance) {
+           balance -= amount;
+           std::cout << "Withdrawn " << amount << " from account.\n";
+        } else {
+           std::cout << "Insufficient balance to withdraw that amount.\n";
+        }
     }
 
     // Display account information
     void DisplayAccountInfo() {
-        // TODO: Implement the DisplayAccountInfo method
+        // Implement the DisplayAccountInfo method
+        std::cout << "Account Number: " << accountNumber << std::endl;
+        std::cout << "Account Holder: " << accountHolderName << std::endl;
+        std::cout << "Balance: " << balance << std::endl;
     }
 
     double GetBalance() const {
--- factorial.hpp
+++ factorial.hpp
@@ -2,8 +2,16 @@
 
 // Function to calculate factorial using multiple threads
 long calculateFactorial(int n) {
-    long result = 0;
-    //TODO: Implement the function to calculate factorial of n
+    long result = 1;
+    
+    //Implement the function to calculate factorial of n
+    if (n < 0) {
+        throw std::invalid_argument("The factorial for negative numbers is not defined");
+    }
+
+    for (int i = 1; i <= n; ++i) {
+        result *= i;
+    }
 
     return result;
 }
